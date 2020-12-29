28ee1a4675b21fc68cb41e05228f519495050969
UCQ <zerda.elsword@gmail.com>
2020-12-29 15:41:52 +0900

    add: errorメッセージがエラーの場所を示すように

--- cc.c
+++ cc.c
@@ -24,10 +24,16 @@ Token *token;
 
 //エラーを報告するための関数
 //printfと同じ引数を取る
-void error(char *fmt, ...)
+
+char *user_input;
+void error_at(char *loc, char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " "); //pos個の空白を出力する
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
